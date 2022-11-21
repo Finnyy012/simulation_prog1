02ec89f920d749e7af74338c79acd523259538d3
Finnyy012 <boris.degraaf@student.hu.nl>
2022-11-21 15:37:24 +0100

    graaf kan nu gevisualiseerd worden

--- main.py
+++ main.py
@@ -1,28 +1,77 @@
 import math
 import numpy as np
+import graphviz
 
-# dit is een FSM
-def FSM(rules: [], tape: [], state: int, accepting: bool):
+#evalueert de FSM
+def eval_FSM(rules: [], tape: [], accepting):
+    state = rules[0][0]
     for condition in tape:
         print(condition)
         for rule in rules:
             if((state == rule[0]) and (condition == rule[1])):
                 state = rule[2]
-                accepting = rule[3]
-                print(str(state) + ' ' + str(accepting))
+                print(str(state) + ' ' + str(state in accepting))
                 break
-    return accepting
+    return state in accepting
 
-rules = [
-    (0,1,1,0),
-    (1,1,0,0),
-    (1,0,2,0),
-    (2,0,1,0),
-    (2,1,3,1),
-    (3,1,2,0),
-    (3,0,0,0),
-    (0,0,3,1)
-]
+##visualiseert de graaf
+def graph(rules, accepting):
+    dot = graphviz.Digraph('FSM')
+    dot.graph_attr['rankdir'] = 'LR'
+    nodes = {}
+    n_node = 65
 
-print(FSM(rules, [0,1,1], 0, 0))
+    start = rules[0][0]
+    for rule in rules:
+        if(start in rule):
+            if(start == rule[0]):
+                rule[0] = '>' + str(rule[0])
+            if(start == rule[2]):
+                rule[2] = '>' + str(rule[2])
+    print(rules)
+
+    for rule in rules:
+        node1 = str(rule[0])
+        node2 = str(rule[2])
+
+        if(node1 not in nodes):
+            nodes[node1] = chr(n_node)
+            n_node+=1
+            if(node1 not in accepting):
+                dot.node(nodes[node1], node1)
+            else:
+                dot.node(nodes[node1], node1, shape='doublecircle')
+
+        if(node2 not in nodes):
+            nodes[node2] = chr(n_node)
+            n_node += 1
+            if(node2 not in accepting):
+                dot.node(nodes[node2], node2)
+            else:
+                dot.node(nodes[node2], node2, shape='doublecircle')
+
+        dot.edge(nodes[node1], nodes[node2], label=str(rule[1]))
+    return dot
+
+##string w/ oneven 'a' en even 'b'
+def test1():
+    rules = [
+        ['XV','b','XX'],
+        ['XX','b','XV'],
+        ['XX','a','VX'],
+        ['VX','a','XX'],
+        ['VX','b','VV'],
+        ['VV','b','VX'],
+        ['VV','a','XV'],
+        ['XV','a','VV']]
+
+    accepting = ['VV']
+    tape = ['a','b','b']
+
+    g = graph(rules, accepting)
+    print(g.source)
+    g.format = 'pdf'
+    g.render(directory='graphviz_renders', view=True)
+
+    print(eval_FSM(rules, tape, accepting))
 
